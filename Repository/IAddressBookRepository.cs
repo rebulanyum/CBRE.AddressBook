97f36ccd7a2d19b9b15128a9faa0f45fd86e56eb
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-11-17 17:12:22 +0300

    Added necessary repository base/main structures.

---
+++ Repository/IAddressBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBRE.AddressBook.Repository
+{
+    public interface IAddressBookRepository
+    {
+        IEnumerable<Person> GetAllPersons(); 
+    }
+}
---
+++ Repository/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBRE.AddressBook.Repository
+{
+    public enum Gender { Male, Female }
+    public sealed class Person
+    {
+        public string FullName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
