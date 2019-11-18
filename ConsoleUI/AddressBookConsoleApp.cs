4d4892839aee30ea46bd6a8fb2996535086aae3d
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-11-18 10:03:52 +0300

    Created the base Console application structure.

---
+++ ConsoleUI/AddressBookConsoleApp.cs
@@ -0,0 +1,35 @@
+﻿using CBRE.AddressBook.Business;
+using CBRE.AddressBook.Repository;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBRE.AddressBook.ConsoleUI
+{
+    public class AddressBookConsoleApp
+    {
+        const string HelpFlag = "-? |-h |--help";
+        private CommandLineApplication app;
+        public AddressBookConsoleApp(CommandLineApplication app)
+        {
+            this.app = app;
+
+            CommandOption helpOption = app.HelpOption(HelpFlag);
+            helpOption.Inherited = true;
+
+            app.FullName = typeof(Program).FullName;
+            
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 0;
+            });
+        }
+
+        public int Run(string[]args)
+        {
+            return this.app.Execute(args);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using CBRE.AddressBook.Business;
+using CBRE.AddressBook.Repository;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CBRE.AddressBook.ConsoleUI
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            
+            var serviceProvider = SetupServices();
+
+            var app = serviceProvider.GetService<AddressBookConsoleApp>();
+            return app.Run(args);
+        }
+
+        static ServiceProvider SetupServices()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton((sp) => new CommandLineApplication(false))
+                .AddTransient<AddressBookConsoleApp>()
+            .BuildServiceProvider();
+
+            return serviceProvider;
         }
     }
 }
