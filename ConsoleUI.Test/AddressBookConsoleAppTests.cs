3e79fcd65ee149e77476962b63203bda0ac351a1
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-11-19 13:07:31 +0300

    Console application is implemented to accept necessary arguments together with commands. Basic/Success unit tests are added.

---
+++ ConsoleUI.Test/AddressBookConsoleAppTests.cs
@@ -0,0 +1,118 @@
+﻿using CBRE.AddressBook.Business;
+using CBRE.AddressBook.ConsoleUI;
+using CBRE.AddressBook.Repository;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI.Test
+{
+    [TestClass]
+    public class AddressBookConsoleAppTests
+    {
+        Mock<IAddressBookBusiness> addressBookBusinessMock;
+        AddressBookConsoleApp addressBookConsoleApp;
+        static Person[] persons;
+
+        [ClassInitialize]
+        public static void InitializeForAllTests(TestContext testContext)
+        {
+            persons = new Person[]
+            {
+                new Person() { ID = 1, FullName = "Richard Azul", Gender = Gender.Male, BirthDate = DateTime.Today.AddYears(-30) },
+                new Person() { ID = 2, FullName = "Clara White", Gender = Gender.Female, BirthDate = DateTime.Today.AddYears(-30).AddDays(-1) }
+            };
+        }
+
+        [TestInitialize]
+        public void InitializeForTest()
+        {
+            addressBookBusinessMock = new Mock<IAddressBookBusiness>();
+            addressBookBusinessMock.Setup(abr => abr.CalculateAgeDifferenceInDays(It.IsAny<int>(), It.IsAny<int>())).Returns(1);
+            addressBookBusinessMock.Setup(abr => abr.CountGenders(It.IsAny<Gender>())).Returns(1);
+            addressBookBusinessMock.Setup(abr => abr.FindOldest()).Returns(persons[0]);
+            addressBookBusinessMock.Setup(abr => abr.FindPersonByName(It.IsAny<string>())).Returns(persons);
+            addressBookBusinessMock.Setup(abr => abr.GetPersonByID(It.IsAny<int>())).Returns(persons[0]);
+            addressBookConsoleApp = new AddressBookConsoleApp(new CommandLineApplication(false), addressBookBusinessMock.Object);
+        }
+
+        [TestMethod]
+        public void CountGenderCommandShouldWork()
+        {
+            int result = -1;
+            try
+            {
+                result = addressBookConsoleApp.Run("countgender", "mAlE");
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                addressBookBusinessMock.Verify(abb => abb.CountGenders(Gender.Male), Times.Once);
+                Assert.AreEqual(0, result);
+            }
+        }
+
+        [TestMethod]
+        public void FindOldestCommandShouldWork()
+        {
+            int result = -1;
+            try
+            {
+                result = addressBookConsoleApp.Run("findoldest");
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                addressBookBusinessMock.Verify(abb => abb.FindOldest(), Times.Once);
+                Assert.AreEqual(0, result);
+            }
+        }
+
+        [TestMethod]
+        public void CalculateAgeDiffCommandShouldWork()
+        {
+            int result = -1;
+            try
+            {
+                result = addressBookConsoleApp.Run("calculateagediff", "1", "2");
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                addressBookBusinessMock.Verify(abb => abb.CalculateAgeDifferenceInDays(1, 2), Times.Once);
+                Assert.AreEqual(0, result);
+            }
+        }
+
+        [TestMethod]
+        public void FindPersonCommandShouldWork()
+        {
+            int result = -1;
+            try
+            {
+                result = addressBookConsoleApp.Run("findperson", "-n", "AsDaSd");
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                addressBookBusinessMock.Verify(abb => abb.FindPersonByName("AsDaSd"), Times.Once);
+                Assert.AreEqual(0, result);
+            }
+        }
+    }
+}
--- ConsoleUI/AddressBookConsoleApp.cs
+++ ConsoleUI/AddressBookConsoleApp.cs
@@ -11,9 +11,11 @@ namespace CBRE.AddressBook.ConsoleUI
     {
         const string HelpFlag = "-? |-h |--help";
         private CommandLineApplication app;
-        public AddressBookConsoleApp(CommandLineApplication app)
+        private IAddressBookBusiness addressBookBusiness;
+        public AddressBookConsoleApp(CommandLineApplication app, IAddressBookBusiness addressBookBusiness)
         {
             this.app = app;
+            this.addressBookBusiness = addressBookBusiness;
 
             CommandOption helpOption = app.HelpOption(HelpFlag);
             helpOption.Inherited = true;
@@ -25,11 +27,134 @@ namespace CBRE.AddressBook.ConsoleUI
                 app.ShowHelp();
                 return 0;
             });
+
+            ConfigureForCountGenderCommand(app);
+            ConfigureForCalculateAgeDifferenceCommand(app);
+            ConfigureForFindOldestCommand(app);
+            ConfigureForFindUserByNameCommand(app);
         }
 
-        public int Run(string[]args)
+        public int Run(params string[]args)
         {
             return this.app.Execute(args);
+        }
+
+        private static string ToString(Person person)
+        {
+            return $"{person.ID},{person.FullName},{person.Gender},{person.BirthDate.ToShortDateString()}";
+        }
+
+        private void ConfigureForCountGenderCommand(CommandLineApplication cla)
+        {
+            cla.Command("countgender", (application) =>
+            {
+                application.Description = "With this command you can count the people with the given gender";
+                CommandArgument genderArgument = application.Argument("gender", "The gender of Person to look for: Male | Female", (argument) =>
+                {
+                    argument.ShowInHelpText = true;
+                    argument.Values.Add("Male");
+                    argument.Values.Add("Female");
+                });
+                application.OnExecute(() =>
+                {
+                    if (cla.OptionHelp.HasValue())
+                    {
+                        application.ShowHelp("countgender");
+                        return 0;
+                    }
+
+                    var gender = Enum.Parse<Gender>(genderArgument.Value, true);
+                    uint count = addressBookBusiness.CountGenders(gender);
+                    Console.WriteLine($"There are {count} {gender}s.");
+                    return 0;
+                });
+            });
+        }
+
+        private void ConfigureForFindOldestCommand(CommandLineApplication cla)
+        {
+            cla.Command("findoldest", (application) =>
+            {
+                application.Description = "With this command you can find the oldest person.";
+                application.OnExecute(() =>
+                {
+                    if (cla.OptionHelp.HasValue())
+                    {
+                        application.ShowHelp("findoldest");
+                        return 0;
+                    }
+                    
+                    Person person = addressBookBusiness.FindOldest();
+                    Console.WriteLine(ToString(person));
+                    return 0;
+                });
+            });
+        }
+
+        private void ConfigureForCalculateAgeDifferenceCommand(CommandLineApplication cla)
+        {
+            cla.Command("calculateagediff", (application) =>
+            {
+                application.Description = "With this command you can calculate the age difference between the given 2 people. The result will be presented in days; If the 1st person is older than 2nd one the result will be positive. If 2nd person is older than 1st person the result will be negative. Otherwise, the result will be 0.";
+                CommandArgument idArgument1 = application.Argument("personID1", "The ID of Person.", (argument) =>
+                {
+                    argument.ShowInHelpText = true;
+                });
+                CommandArgument idArgument2 = application.Argument("personID2", "The ID of Person.", (argument) =>
+                {
+                    argument.ShowInHelpText = true;
+                });
+                application.OnExecute(() =>
+                {
+                    if (cla.OptionHelp.HasValue())
+                    {
+                        application.ShowHelp("calculateagediff");
+                        return 0;
+                    }
+
+                    bool parseSuccess;
+                    int person1ID, person2ID;
+                    parseSuccess = int.TryParse(idArgument1.Value, out person1ID);
+                    parseSuccess = parseSuccess & int.TryParse(idArgument2.Value, out person2ID);
+                    if (!parseSuccess)
+                    {
+                        application.Error.WriteLineAsync("The given arguments cannot be parsed.");
+                        return -1;
+                    }
+
+                    int diff = addressBookBusiness.CalculateAgeDifferenceInDays(person1ID, person2ID);
+                    Console.WriteLine($"The age difference between the 2 people is {diff}");
+                    return 0;
+                });
+            });
+        }
+
+        private void ConfigureForFindUserByNameCommand(CommandLineApplication cla)
+        {
+            cla.Command("findperson", (application) =>
+            {
+                application.Description = "With this command you can search for the people's names. The search is not case-sensitive and it starts searching from beginning of the people's fullname.";
+                CommandOption nameOption = application.Option("-n |--Name", "The name of Person.", CommandOptionType.SingleValue, (option) =>
+                {
+                    option.ShowInHelpText = true;
+                });
+                application.OnExecute(() =>
+                {
+                    if (cla.OptionHelp.HasValue())
+                    {
+                        application.ShowHelp("findperson");
+                        return 0;
+                    }
+
+                    string personName = nameOption.HasValue() ? nameOption.Value() : string.Empty;
+                    IEnumerable<Person> persons = addressBookBusiness.FindPersonByName(personName);
+                    foreach (Person person in persons)
+                    {
+                        Console.WriteLine(ToString(person));
+                    }
+                    return 0;
+                });
+            });
         }
     }
 }
