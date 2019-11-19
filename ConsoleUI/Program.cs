58929444e859616253e52f409645a60964afe5de
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-11-19 10:38:01 +0300

    Added configuration file for the ConsoleUI and do the configuration.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,8 +1,10 @@
 ﻿using CBRE.AddressBook.Business;
 using CBRE.AddressBook.Repository;
 using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 
 namespace CBRE.AddressBook.ConsoleUI
 {
@@ -10,20 +12,24 @@ namespace CBRE.AddressBook.ConsoleUI
     {
         public static int Main(string[] args)
         {
-            var serviceProvider = SetupServices();
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var serviceProvider = SetupServices(config);
 
             var app = serviceProvider.GetService<AddressBookConsoleApp>();
             return app.Run(args);
         }
 
-        static ServiceProvider SetupServices()
+        static ServiceProvider SetupServices(IConfiguration config)
         {
             var serviceProvider = new ServiceCollection()
                 .AddSingleton((sp) => new CommandLineApplication(false))
                 .AddTransient<AddressBookConsoleApp>()
                 .AddTransient<IAddressBookBusiness, AddressBookBusiness>()
-                .AddTransient<IAddressBookRepository, AddressBookCsvRepository>()
-
+                .AddTransient<IAddressBookRepository, AddressBookCsvRepository>(sp => new AddressBookCsvRepository(config.GetSection("AddressBook").GetSection("DataSource").Value))
             .BuildServiceProvider();
 
             return serviceProvider;
